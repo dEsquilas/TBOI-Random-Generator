8f2462fa82ac1ba65ffdb712479ad9366c6dd60e
Dani Esquilas <desquilas@gmail.com>
2026-01-04 14:57:04 +0100

    feat(display): show spinning players and objectives during animation

--- src/components/display/FullDisplay.vue
+++ src/components/display/FullDisplay.vue
@@ -6,10 +6,35 @@ import PlayerResult from './PlayerResult.vue'
 const store = useGameStore()
 
 const hasResults = computed(() => store.results.players.length > 0)
+const isSpinning = computed(() => store.isSpinning)
+const spinningPlayers = computed(() => store.spinningPlayers)
+const spinningObjectives = computed(() => store.spinningObjectives)
 const players = computed(() => store.results.players)
 const objectives = computed(() => store.results.objectives)
 const challenges = computed(() => store.results.challenges)
 const numPlayers = computed(() => store.numberPlayers)
+
+// Show spinning players during animation, otherwise show results or placeholder
+const displayPlayers = computed(() => {
+  if (isSpinning.value && spinningPlayers.value.length > 0) {
+    return spinningPlayers.value
+  }
+  if (hasResults.value) {
+    return players.value
+  }
+  return null
+})
+
+// Show spinning objectives during animation, otherwise show results
+const displayObjectives = computed(() => {
+  if (isSpinning.value && spinningObjectives.value.length > 0) {
+    return spinningObjectives.value
+  }
+  if (hasResults.value) {
+    return objectives.value
+  }
+  return null
+})
 </script>
 
 <template>
@@ -18,10 +43,10 @@ const numPlayers = computed(() => store.numberPlayers)
 
     <!-- Players -->
     <div class="absolute top-[27%] left-1/2 -translate-x-1/2 flex justify-center gap-4">
-      <template v-if="hasResults">
+      <template v-if="displayPlayers">
         <PlayerResult
-          v-for="(player, idx) in players"
-          :key="player.id"
+          v-for="(player, idx) in displayPlayers"
+          :key="`${player.id}-${idx}`"
           :player="player"
           :size="numPlayers > 2 ? 'small' : 'normal'"
           :show-name="numPlayers < 4"
@@ -38,10 +63,10 @@ const numPlayers = computed(() => store.numberPlayers)
     </div>
 
     <!-- Objectives -->
-    <div v-if="hasResults" class="absolute bottom-[25%] left-1/2 -translate-x-1/2 flex gap-2">
+    <div v-if="displayObjectives" class="absolute bottom-[25%] left-1/2 -translate-x-1/2 flex gap-2">
       <img
-        v-for="obj in objectives"
-        :key="obj.id"
+        v-for="(obj, idx) in displayObjectives"
+        :key="`${obj.id}-${idx}`"
         :src="`/img/${obj.image}`"
         :alt="obj.name"
         class="max-w-[100px] h-auto"
@@ -49,7 +74,7 @@ const numPlayers = computed(() => store.numberPlayers)
     </div>
 
     <!-- Challenges -->
-    <div v-if="challenges.length > 0" class="absolute bottom-[5%] left-1/2 -translate-x-1/2 flex gap-2">
+    <div v-if="challenges.length > 0 && !isSpinning" class="absolute bottom-[5%] left-1/2 -translate-x-1/2 flex gap-2">
       <img
         v-for="ch in challenges"
         :key="ch.id"
